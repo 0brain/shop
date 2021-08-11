6df72a5d24331450a6757a1c8f363a976d06cb31
nazar <nazar.khort@gmail.com>
2021-08-11 11:00:21 +0300

    Ввів декоратор і функцію create для обробки сторінки додавання товарів в таблицю.

--- main.py
+++ main.py
@@ -23,5 +23,10 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/create')
+def create():
+    return render_template("create.html")
+
+
 if __name__=='__main__':
     app.run(debug=True)
